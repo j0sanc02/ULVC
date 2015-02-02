b7e664a9849265d7faa6c38c7a834d2bebfa545d
j0sanc02 <j0sanc02@louisville.edu>
2015-02-02 12:25:49 -0500

    Initial Layout and Homepage Added
    Also includes the beginning of an inventory management system.

--- UniversityofLouisvilleVaccine/Models/AccountViewModels.cs
+++ UniversityofLouisvilleVaccine/Models/AccountViewModels.cs
@@ -15,16 +15,13 @@ namespace UniversityofLouisvilleVaccine.Models
 
     public class ManageUserViewModel
     {
-
  
         [Display(Name = "Role")]
         public string Role { get; set; }
-
        
         [DataType(DataType.Password)]
         [Display(Name = "Current password")]
         public string OldPassword { get; set; }
-
 
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
