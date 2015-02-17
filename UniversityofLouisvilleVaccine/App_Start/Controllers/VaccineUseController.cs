013659bae23e9cd79d2bb87d0bb6ee1751c310f2
j0sanc02 <j0sanc02@louisville.edu>
2015-02-17 14:37:18 -0500

    Added Vendor and VaccineUse MVC

---
+++ UniversityofLouisvilleVaccine/App_Start/Controllers/VaccineUseController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using UniversityofLouisvilleVaccine.Models;
+
+namespace UniversityofLouisvilleVaccine.App_Start.Controllers
+{
+    public class VaccineUseController : Controller
+    {
+        private VaccineUseDBContext db = new VaccineUseDBContext();
+
+        // GET: /VaccineUse/
+        public ActionResult Index()
+        {
+            return View(db.VaccineUses.ToList());
+        }
+
+        // GET: /VaccineUse/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            VaccineUse vaccineuse = db.VaccineUses.Find(id);
+            if (vaccineuse == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vaccineuse);
+        }
+
+        // GET: /VaccineUse/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /VaccineUse/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="VaccineUseId,vaccineID,lotNumber,patientID,injectionSite,quantity")] VaccineUse vaccineuse)
+        {
+            if (ModelState.IsValid)
+            {
+                db.VaccineUses.Add(vaccineuse);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(vaccineuse);
+        }
+
+        // GET: /VaccineUse/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            VaccineUse vaccineuse = db.VaccineUses.Find(id);
+            if (vaccineuse == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vaccineuse);
+        }
+
+        // POST: /VaccineUse/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="VaccineUseId,vaccineID,lotNumber,patientID,injectionSite,quantity")] VaccineUse vaccineuse)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(vaccineuse).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(vaccineuse);
+        }
+
+        // GET: /VaccineUse/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            VaccineUse vaccineuse = db.VaccineUses.Find(id);
+            if (vaccineuse == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vaccineuse);
+        }
+
+        // POST: /VaccineUse/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            VaccineUse vaccineuse = db.VaccineUses.Find(id);
+            db.VaccineUses.Remove(vaccineuse);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
